c8d9a29cd52e8bbdd440fcd72b621bff5abd5c67
Jorge Felipe <jorge.ft.mendonca@gmail.com>
2026-04-16 04:26:52 -0300

    Mini Ajuste Model Avaliação
    Foi feito um ajuste no código do Model de Avaliação para se adequar ao modelo usado pelo professor.

--- models/avaliacao.js
+++ models/avaliacao.js
@@ -1,24 +1,46 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Avaliacao extends Model {
-    
+import { Model, DataTypes } from 'sequelize';
+import sequelize from "./Database.js";
+
+export default class Avaliacao extends Model {
+
     static associate(models) {
-      Avaliacao.belongsTo(models.Pedido, {
-        foreignKey: 'pedidoId',
-        as: 'pedido'
-      })
+        Avaliacao.belongsTo(models.Pedido, {
+            foreignKey: 'pedidoId',
+            as: 'pedido'
+        });
     }
-  }
-  Avaliacao.init({
-    nota: DataTypes.INTEGER,
-    pedidoId: DataTypes.INTEGER
-  }, {
+
+}
+
+Avaliacao.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    nota: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5
+      }
+    },
+
+    pedidoId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  },
+  {
     sequelize,
     modelName: 'Avaliacao',
-  });
-  return Avaliacao;
-};
+    tableName: 'avaliacoes',
+    paranoid: true,
+    timestamps: true
+  }
+);
+
 //67!
